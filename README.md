15f532e762b17b67bbf9cbaf4dcc66f8665840e1
YonghoChoi <yongho1037@gmail.com>
2020-08-22 01:15:13 +0900

    Azure App Center 연동

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nomad-cafe-app
+[![Build status](https://build.appcenter.ms/v0.1/apps/cf07d19e-4e25-48f4-9db1-a938d370d0b2/branches/master/badge)](https://appcenter.ms)
+
 디프만 8기에서 진행한 주변 작업하기 좋은 카페 찾기 어플입니다.
--- app/src/main/java/com/yonghochoi/nomadcafeapp/MainActivity.java
+++ app/src/main/java/com/yonghochoi/nomadcafeapp/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 import static com.yonghochoi.nomadcafeapp.R.id.webView;
 
@@ -20,29 +23,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 웹뷰 셋팅
-        mWebView = (WebView) findViewById(webView);//xml 자바코드 연결
-        mWebView.getSettings().setJavaScriptEnabled(true);//자바스크립트 허용
+        AppCenter.start(getApplication(), "70d4356b-d309-464b-b839-d79e88d985a8",
+                Analytics.class, Crashes.class);
 
-        mWebView.loadUrl("https://workingspace.netlify.app/");//웹뷰 실행
-        mWebView.setWebChromeClient(new WebChromeClient());//웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
-        mWebView.setWebViewClient(new WebViewClientClass());//새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사용
+        // 웹뷰 셋팅
+        mWebView = (WebView) findViewById(webView);             //xml 자바코드 연결
+        mWebView.getSettings().setJavaScriptEnabled(true);      //자바스크립트 허용
+
+        mWebView.loadUrl("https://workingspace.netlify.app/");  //웹뷰 실행
+        mWebView.setWebChromeClient(new WebChromeClient());     //웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
+        mWebView.setWebViewClient(new WebViewClientClass());    //새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사용
 
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {//뒤로가기 버튼 이벤트
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {//웹뷰에서 뒤로가기 버튼을 누르면 뒤로가짐
+    public boolean onKeyDown(int keyCode, KeyEvent event) {                     //뒤로가기 버튼 이벤트
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {       //웹뷰에서 뒤로가기 버튼을 누르면 뒤로가짐
             mWebView.goBack();
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
 
-    private class WebViewClientClass extends WebViewClient {//페이지 이동
+    private class WebViewClientClass extends WebViewClient {        //페이지 이동
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            Log.d("check URL",url);
+            Log.d("check URL", url);
             view.loadUrl(url);
             return true;
         }
