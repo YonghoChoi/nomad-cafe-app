21c9b84dc86072541dbde08d78c011e042cb66b7
YonghoChoi <yongho1037@gmail.com>
2020-08-27 22:13:31 +0900

    어플에서 GPS 사용 가능하도록 설정 변경

--- app/src/main/java/com/yonghochoi/nomadcafeapp/MainActivity.java
+++ app/src/main/java/com/yonghochoi/nomadcafeapp/MainActivity.java
@@ -2,10 +2,11 @@ package com.yonghochoi.nomadcafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import com.microsoft.appcenter.AppCenter;
@@ -17,23 +18,35 @@ import static com.yonghochoi.nomadcafeapp.R.id.webView;
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppCenter.start(getApplication(), "70d4356b-d309-464b-b839-d79e88d985a8",
+        String appSecret = System.getenv("APP_CENTER_SECRET");
+
+        AppCenter.start(getApplication(), appSecret,
                 Analytics.class, Crashes.class);
 
         // 웹뷰 셋팅
         mWebView = (WebView) findViewById(webView);             //xml 자바코드 연결
         mWebView.getSettings().setJavaScriptEnabled(true);      //자바스크립트 허용
         mWebView.getSettings().setDatabaseEnabled(true);
-        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);  // 로컬 저장소 허용
+        mWebView.getSettings().setGeolocationEnabled(true); // GPS 허용
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);  // 자바스크립트 새창 띄우기 허용 여부
+        mWebView.getSettings().setUseWideViewPort(true);    // 화면 사이즈 맞추기 허용
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용안함
+        mWebView.getSettings().setSupportMultipleWindows(true); // 새창 띄우기 여부
+        mWebView.getSettings().setBuiltInZoomControls(true);    // 화면 확대/축소 허용
+        mWebView.getSettings().setSupportZoom(true);    // 줌 허용
+        mWebView.getSettings().setLoadWithOverviewMode(true);   // 메타 태그 허용
+        mWebView.setWebChromeClient(new GeoWebChromeClient());
 
         mWebView.loadUrl("https://workingspace.netlify.app/");  //웹뷰 실행
-        mWebView.setWebChromeClient(new WebChromeClient());     //웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
-        mWebView.setWebViewClient(new WebViewClientClass());    //새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사용
+//        mWebView.setWebChromeClient(new WebChromeClient());     //웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
+//        mWebView.setWebViewClient(new WebViewClientClass());    //새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사용
 
     }
 
@@ -56,3 +69,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
